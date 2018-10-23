19df4751873b5f4486900c54ec8e5415af96b2cf
Jay Mundrawala <jay@thechamberofunderstanding.com>
2018-10-23 16:01:31 -0500

    add hab sup term issue test

---
+++ habitat/plan.sh
@@ -0,0 +1,14 @@
+pkg_name=term-test
+pkg_origin=jaym
+pkg_version="0.1.0"
+pkg_maintainer="The Habitat Maintainers <humans@habitat.sh>"
+pkg_license=("Apache-2.0")
+pkg_deps=(core/bash)
+
+do_build() {
+	return 0
+}
+
+do_install() {
+	return 0
+}
